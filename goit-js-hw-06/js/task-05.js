eaa2c24fa33f9f2eda5bbb8fe138ccb3a0ecb353
NastyaK02 <124596623+NastyaK02@users.noreply.github.com>
2023-06-23 16:24:23 +0300

    Delete goit-js-hw-06 directory

--- goit-js-hw-06/js/task-05.js
+++
@@ -1,9 +0,0 @@
-const userName = document.querySelector("#name-input");
-const spanElem = document.querySelector("#name-output");
-
-userName.addEventListener("input", (event) => {
- if (userName.value) {
-  return (spanElem.textContent = event.currentTarget.value);
- }
- return (spanElem.textContent = "Anonymous");
-});
--- goit-js-hw-06/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputElem = document.querySelector("#font-size-control");
-const textElem = document.querySelector("#text");
-
-inputElem.addEventListener("input", oninput);
-
-function oninput(evt) {
- const fontSize = inputElem.value + "px";
- textElem.style.fontSize = fontSize;
-}
