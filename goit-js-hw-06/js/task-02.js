36547d480fc33145aea1cb12a3508b5ff2401b08
NastyaK02 <124596623+NastyaK02@users.noreply.github.com>
2023-06-23 16:14:43 +0300

    update

--- goit-js-hw-06/js/task-02.js
+++ goit-js-hw-06/js/task-02.js
@@ -7,12 +7,12 @@ const ingredients = [
  "Condiments",
 ];
 
-const ulElem = document.querySelector("#ingredients");
+const ulEl = document.querySelector("#ingredients");
 
 ingredients.forEach((ingredient) => {
  const itemElem = document.createElement("li");
  itemElem.classList.add("item");
  itemElem.textContent = ingredient;
- ulElem.appendChild(itemElem);
+ ulEl.appendChild(itemElem);
 });
-console.log(ulElem);
+console.log(ulEl);
